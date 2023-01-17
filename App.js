7263de8703c5940d41c5d36ba42e115cc401c2bb
kavishgupta1802 <kavishgupta2011@gmail.com>
2023-01-17 22:28:57 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+function changecolor1() {
+    alert("Button 1 Clicked!");
+    const btn = document.getElementById('circle1');
+    btn.style.backgroundColor = 'salmon';
+}
+function changecolor2() {
+    alert("Button 2 Clicked!");
+    const btn = document.getElementById('circle2');
+    btn.style.backgroundColor = 'salmon';
+}
+function changecolor3() {
+    alert("Button 3 Clicked!");
+    const btn = document.getElementById('circle3');
+    btn.style.backgroundColor = 'salmon';
+}
